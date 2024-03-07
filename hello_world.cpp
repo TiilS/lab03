09397898e3ee90a52a02df8410750ef2e9954891
TiilS <75080702+TiilS@users.noreply.github.com>
2024-03-07 10:42:07 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main(){
-    std::string name; // инициализация переменной 
-    std::cin >> name; // получение значение переменной из стандартного потока ввода
-    std::cout << "Hello world from " << name << std::endl; // вывод сообщения
+    std::string name; // инициализация переменной.
+    std::cin >> name; // получение значение переменной из стандартного потока ввода.
+    std::cout << "Hello world from " << name << std::endl; // вывод сообщения.
     return 0;
 }
