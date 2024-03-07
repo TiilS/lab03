d2b138e997fe52a73aa7e82cd6cba4557162496a
TiilS <iagildin@mail.ru>
2024-03-07 09:17:13 +0300

    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main(){
-    std::string name;
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    std::string name; // инициализация переменной 
+    std::cin >> name; // получение значение переменной из стандартного потока ввода
+    std::cout << "Hello world from " << name << std::endl; // вывод сообщения
     return 0;
 }
